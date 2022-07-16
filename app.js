5f11ce7493fe4a8cc498420f1c93dc900fd78367
AMRESH <amreshaiio@gmail.com>
2022-07-17 02:13:58 +0530

    CSS, EJS partials, Addbutton

--- app.js
+++ app.js
@@ -6,24 +6,34 @@ app.set('view engine', 'ejs');                      // set the view engine to ej
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));                  //Serve  css file
 
-var items = [];                                     //1] declare empty array named items
+let items = [];                                     //1] declare empty array for personal list
+let workItems = [];                                 //declare empty array for work list
+
+//________________________________________________________________________TO RENDER WORK LIST
 
 app.get('/', (req, res) => {
-
-    //Get day, date
-    var today = new Date();
-    var options = { weekday: "long", day: "numeric", month: "long"};
-    var day = today.toLocaleDateString("en-us", options);
-
-    res.render("index", {kindofDay: day, newlistItems: items});     //5] Renders homepage 
+    res.render("listpage", {listTitle: "Personal list", newlistItems: items, url: "/"});     //5] Renders homepage 
 });
 
 app.post('/', (req, res) => {
-    var item = req.body.newItem;                //2] Capture form data
+    let item = req.body.newItem;                //2] Capture form data
     items.push(item);                           //3] pushing form data to items array for every add button click in index.ejs file
     res.redirect('/');                          //4] Redirect you to home page.
 });
 
+
+
+//________________________________________________________________________TO RENDER WORK LIST
+
+app.get("/work", (req, res) => {
+    res.render("listpage", {listTitle: "Work list", newlistItems: workItems, url: "/work"});
+});
+
+app.post("/work", (req, res) => {
+    let item = req.body.newItem; 
+    workItems.push(item);
+    res.redirect('/work');
+});
 
 
 
